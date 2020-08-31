6d59ee02c44112860991ea04a14ed58a3daef071
jankubatt <jankubat.1@seznam.cz>
2020-08-31 10:00:53 +0200

    heroku

--- index.js
+++ index.js
@@ -26,3 +26,5 @@ bot.on('message', message => {
 });
 
 bot.login(token);
+
+const http = require('http');const port = process.env.PORT || 80;const server = http.createServer((req, res) => {res.statusCode = 200;res.setHeader('Content-Type', 'text/html');res.end('BalinBot is running');});server.listen(port,() => {console.log(`Server running at port `+port);});
