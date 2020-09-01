4718a14222ab1094f15bf0bf99b1edd359ae10ca
jankubatt <jankubat.1@seznam.cz>
2020-09-01 13:47:44 +0200

    Heroku removed

--- index.js
+++ index.js
@@ -128,6 +128,3 @@ function play(guild, song) {
 }
 
 client.login(token);
-
-//heroku shit
-const http = require('http');const port = process.env.PORT || 80;const server = http.createServer((req, res) => {res.statusCode = 200;res.setHeader('Content-Type', 'text/html');res.end('BalinBot is running');});server.listen(port,() => {console.log(`Server running at port `+port);});
