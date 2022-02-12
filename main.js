7d5f622a756ec5e6606ce0af150d7acd99d6ef49
jankubatt <jankubat77@gmail.com>
2022-02-12 17:11:41 +0100

    school break checking

--- main.js
+++ main.js
@@ -110,9 +110,59 @@ client.on('ready', () => {
 
     checkSupl();
 
+    //Interval for break checking (Edit this, if you have different times of breaks in school)
+    setInterval(() => {
+        let date = new Date();
+
+        if (date.getHours() == 8 && date.getMinutes() == 0) {
+            checkSupl();
+        }
+        else if (date.getHours() == 8 && date.getMinutes() == 45) {
+            checkSupl();
+        }
+        else if (date.getHours() == 8 && date.getMinutes() == 55) {
+            checkSupl();
+        }
+        else if (date.getHours() == 9 && date.getMinutes() == 40) {
+            checkSupl();
+        }
+        else if (date.getHours() == 9 && date.getMinutes() == 50) {
+            checkSupl();
+        }
+        else if (date.getHours() == 10 && date.getMinutes() == 35) {
+            checkSupl();
+        }
+        else if (date.getHours() == 10 && date.getMinutes() == 50) {
+            checkSupl();
+        }
+        else if (date.getHours() == 11 && date.getMinutes() == 35) {
+            checkSupl();
+        }
+        else if (date.getHours() == 11 && date.getMinutes() == 45) {
+            checkSupl();
+        }
+        else if (date.getHours() == 12 && date.getMinutes() == 30) {
+            checkSupl();
+        }
+        else if (date.getHours() == 12 && date.getMinutes() == 40) {
+            checkSupl();
+        }
+        else if (date.getHours() == 13 && date.getMinutes() == 25) {
+            checkSupl();
+        }
+        else if (date.getHours() == 13 && date.getMinutes() == 35) {
+            checkSupl();
+        }
+        else if (date.getHours() == 14 && date.getMinutes() == 20) {
+            checkSupl();
+        }
+
+    }, 1000 * 60 * 2.5)
+
+    //Interval for regular checking
     setInterval(() => {
         checkSupl();
-    }, 1000 * 60 * 10)
+    }, 1000 * 60 * 60)
 });
 
 client.on('messageCreate', (message) => {
